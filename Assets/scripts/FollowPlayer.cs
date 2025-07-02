5df0603964470d8ff87d0fbb7de97311db683a4d
Joelcfn <elkiraque@gmail.com>
2025-07-02 03:48:04 +0100

    Update FollowPlayer.cs
    5.Smooth the Camera with LateUpdate

--- Assets/scripts/FollowPlayer.cs
+++ Assets/scripts/FollowPlayer.cs
@@ -11,7 +11,7 @@ private Vector3 offset = new Vector3(0, 5, -7);
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
     transform.position = player.transform.position + offset;
 
