289e7b77057bdc65addfa204ed95d4c0197f326d
Joelcfn <elkiraque@gmail.com>
2025-07-02 04:18:12 +0100

    Update PlayerController.cs
    5.Clean your code and hierarchy

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,10 +4,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20.0f;
+private float turnSpeed = 45.0f;
+private float horizontalInput;
+private float forwardInput;
 
     void Start()
     {
@@ -17,14 +17,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
   horizontalInput = Input.GetAxis("Horizontal");
-      forwardInput = Input.GetAxis("Vertical");
+    forwardInput = Input.GetAxis("Vertical");
 
-
-   
+    // Moves the car forward based on vertical input
     transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+    // Rotates the car based on horizontal input
     transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-    // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
   }
 }
